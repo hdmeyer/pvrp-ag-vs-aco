46f0f9fd4e1b8cea166a1e401105b7ec34798360
cdparra <cdparra@7294a8cf-013f-0410-b56a-9dc7521f48fc>
2007-11-16 03:27:50 +0000

    Primeros fuentes, estructurados en un proyecto netbeans

---
+++ trunk/GA-PVRP-JAVA/GA-PVRP-JAVA/src/gapvrpjava/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Main.java
+ *
+ * Created on 31 de octubre de 2007, 19:37
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package gapvrpjava;
+
+/**
+ *
+ * @author Huguis
+ */
+public class Main {
+    
+    /** Creates a new instance of Main */
+    public Main() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
