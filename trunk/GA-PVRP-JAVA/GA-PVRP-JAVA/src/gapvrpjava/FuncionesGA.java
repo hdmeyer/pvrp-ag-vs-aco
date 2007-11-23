38207da1c10edac10d0761ee8e8d367b1cf6ce3a
cdparra <cdparra@7294a8cf-013f-0410-b56a-9dc7521f48fc>
2007-11-23 03:40:26 +0000

    Cambios iniciales en la representación de los parámetros de entrada del problema.

--- trunk/GA-PVRP-JAVA/GA-PVRP-JAVA/src/gapvrpjava/FuncionesGA.java
+++ trunk/GA-PVRP-JAVA/GA-PVRP-JAVA/src/gapvrpjava/FuncionesGA.java
@@ -8,48 +8,23 @@
  */
 
 package gapvrpjava;
-
+import java.util.*;
 /**
  *
  * @author Propietario
  */
 public class FuncionesGA {
-    int cromosoma[][][] = null;
     /** Creates a new instance of FuncionesGA */
+    LinkedList<Cromosoma> poblacion = new LinkedList<Cromosoma>();
+    
     public FuncionesGA() {
     }
     /*PODRIAMOS PONER A CERO CUANDO INICIALIZAMOS EL CROMOSOMA*/    
-    public void inicializarCromosoma(Lector entrada){
-        this.cromosoma = new int [entrada.dias][entrada.cantVehiculos][entrada.cantClientes];
-    }
-    public void construirCromosoma(Lector entrada){
-        int contador = 0;
-        int vehiculos[] = new int[entrada.cantVehiculos];
-        for (int i = 0; i < vehiculos.length; i++) {
-            vehiculos[i] = entrada.capacidad;
-        }
-        for (int i = 0; i < entrada.dias; i++) {
-            for (int j = 0; j < entrada.cantClientes; j++) {
-                for (int k = 0; k < entrada.cantVehiculos; k++) {
-                    /*INCREMENTAMOS UN CONTADOR PARA IR VISITANDO A LOS CLIENTES Y METIENDOLOS EN EL CROMOSOMA
-                     LUEGO, SI LA CANTIDAD DE VISITAS QUE LE NECSITAMOS PARA EL CLIENTE AUN NO FUE SATISFECHA,
-                     LO VISITAMOS DE VUELTA Y DECREMENTAMOS LA CANTIDAD DE VISITAS NECESARIAS*/
-                    contador++;
-                    if(entrada.inicial[contador][5] > 0){
-                        if((vehiculos[k]-entrada.inicial[contador][4] >= 0)){
-                            this.cromosoma[i][j][k] =(int)entrada.inicial[contador][0];
-                            entrada.inicial[contador][5]--;
-                        }// falta ver cuando no asignamos al cliente....
-                        
-                    }
-                    if(contador == entrada.cantClientes){
-                        j = entrada.cantClientes;
-                        k = entrada.cantVehiculos;
-                        contador =0;
-                    }
-                }
-                
-            }
+    
+    
+    public void cruce(){
+        ListIterator itr = poblacion.listIterator();
+        for (Cromosoma elem : poblacion) {
             
         }
     }
