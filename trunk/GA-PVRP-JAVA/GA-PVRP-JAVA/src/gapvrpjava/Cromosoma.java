43bd6d25ab7819641e0d0c22b227325e50a61c16
meyer.hugo <meyer.hugo@7294a8cf-013f-0410-b56a-9dc7521f48fc>
2007-12-05 23:29:19 +0000

    Modificacion de la clase conocimiento y cromosoma... faltan todavia algunas cosas..., se borro lector

--- trunk/GA-PVRP-JAVA/GA-PVRP-JAVA/src/gapvrpjava/Cromosoma.java
+++ trunk/GA-PVRP-JAVA/GA-PVRP-JAVA/src/gapvrpjava/Cromosoma.java
@@ -15,11 +15,17 @@ package gapvrpjava;
  */
 public class Cromosoma {
     int cromosoma [][][]=null;
+    int fitness;
+    int listaClientes[][] = null;
     /** Creates a new instance of Cromosoma */
     public Cromosoma() {
     }
+    /** Usaremos aqui un indice que va desde uno hasta la cantidad de clientes!!!, para 
+     *poder manejar con sus indices reales
+     Tanto los clientes como los vehiculos van desde cero hasta su valor menos 1*/
     public Cromosoma(Conocimiento entrada) {
-        this.cromosoma = new int [entrada.dias][entrada.cantVehiculos][entrada.cantClientes];
+        this.cromosoma = new int [entrada.dias][entrada.cantVehiculos][entrada.cantClientes+1];
+        this.listaClientes = new int [2][entrada.cantClientes+1];
     }
     public void construirCromosoma(Conocimiento entrada){
         int contador = 0;
@@ -28,7 +34,7 @@ public class Cromosoma {
             vehiculos[i] = entrada.capacidad;
         }
         for (int i = 0; i < entrada.dias; i++) {
-            for (int j = 0; j < entrada.cantClientes; j++) {
+            for (int j = 1; j < entrada.cantClientes+1; j++) {
                 for (int k = 0; k < entrada.cantVehiculos; k++) {
                     /*INCREMENTAMOS UN CONTADOR PARA IR VISITANDO A LOS CLIENTES Y METIENDOLOS EN EL CROMOSOMA
                      LUEGO, SI LA CANTIDAD DE VISITAS QUE  NECSITAMOS PARA EL CLIENTE AUN NO FUE SATISFECHA,
@@ -36,19 +42,19 @@ public class Cromosoma {
                     /* En este if verificamos si la cantidad de visitas necesarias para ese cliente
                      *aun no fue satisfecha.*/
                     contador++;
-                    if(entrada.inicial[contador][5] > 0){
+                    if(entrada.clientes[contador][5] > 0){
                         
                         /*Verificamos si la demanda del cliente puede ser satisfecha por este vehiculo*/
-                        if((vehiculos[k]-entrada.inicial[contador][4] >= 0)){
+                        if((vehiculos[k]-entrada.clientes[contador][4] >= 0)){
                             /*Si es asi, se carga en el cromosoma el nro de cliente a ser visitado*/
-                            this.cromosoma[i][j][k] =(int)entrada.inicial[contador][0];
-                            entrada.inicial[contador][5]--;
+                            this.cromosoma[i][j][k] =(int)entrada.clientes[contador][0];
+                            entrada.clientes[contador][5]--;
                             
                         }
                         
                     }
-                    if(contador == entrada.cantClientes){
-                        j = entrada.cantClientes;
+                    if(contador == entrada.cantClientes+1){
+                        j = entrada.cantClientes+1;
                         k = entrada.cantVehiculos;
                         contador =0;
                     }
