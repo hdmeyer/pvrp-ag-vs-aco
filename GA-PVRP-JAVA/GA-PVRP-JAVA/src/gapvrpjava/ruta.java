d0bde98a2f063b1b847b063884a518ec7763da9b
meyer.hugo <meyer.hugo@7294a8cf-013f-0410-b56a-9dc7521f48fc>
2007-12-07 17:54:54 +0000

    se agrego la clase ruta y los cambios respectivos que las dmas requieren...

---
+++ GA-PVRP-JAVA/GA-PVRP-JAVA/src/gapvrpjava/ruta.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gapvrpjava;
+
+/**
+ *
+ * @author Cristhian Parra-Hugo Meyer
+ */
+public class ruta {
+    int []ruta;
+    double costo;
+    public void getCosto(){};
+    public ruta(Conocimiento entrada){
+        ruta = new int[entrada.cantClientes+1];
+        
+    }
+    
+
+}
