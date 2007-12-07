319f95b098246779ef727686c5443f0c10c1f662
cdparra <cdparra@7294a8cf-013f-0410-b56a-9dc7521f48fc>
2007-12-07 15:19:11 +0000

    Nueva prueba de la inicialización de la población

--- trunk/GA-PVRP-JAVA/GA-PVRP-JAVA/src/pruebas/TestCromosoma.java
+++ trunk/GA-PVRP-JAVA/GA-PVRP-JAVA/src/pruebas/TestCromosoma.java
@@ -29,13 +29,32 @@ public class TestCromosoma {
         Conocimiento prueba= new Conocimiento();
         String filename = "C:\\pvrp\\p26";
         
+     
         prueba.CargarConocimiento(filename);
         Cromosoma cromo = new Cromosoma(prueba);
         cromo.construirCromosoma(prueba);
+                
+        System.out.println("1. PRUEBA DE GENERACIÓN DEL CROMOSOMA");
+        System.out.println("-* Cromosoma Generado ================================================");
+        
         System.out.println(cromo.ImprimirCromo(cromo.toString(prueba)));
         cromo.fObjetivo(prueba);
-        System.out.println("================================================");
-        System.out.println("FITNESS DEL INDIVIDUO: "+cromo.fitness);
+        System.out.println("=======================================================================");
+        System.out.println("| FITNESS: "+cromo.fitness+"                                          |");
+        System.out.println("=======================================================================");
+        System.out.println();
+        System.out.println("2. PRUEBA DE INICIALIZAR POBLACION");
+        System.out.println("-* Población Inicial Generada =========================================");
+        
+        Poblacion p = new Poblacion(prueba, 30, 30);
+        
+        p.imprimir();
+        
+        System.out.println("=======================================================================");
+        System.out.println("| POBLACIÓN TOSTRING                                                  |");
+        System.out.println("=======================================================================");
+        System.out.println(p.toString());
+        
     }
     
 }
