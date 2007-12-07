229e9cdae217dc0c570b7dfaa25c558045ee4097
meyer.hugo <meyer.hugo@7294a8cf-013f-0410-b56a-9dc7521f48fc>
2007-12-07 05:44:40 +0000

    Se agrega el operador de cruce, faltando todavia la evaluacion de repetidos...

--- GA-PVRP-JAVA/GA-PVRP-JAVA/src/gapvrpjava/FuncionesGA.java
+++ GA-PVRP-JAVA/GA-PVRP-JAVA/src/gapvrpjava/FuncionesGA.java
@@ -24,19 +24,52 @@ public class FuncionesGA {
     /*PODRIAMOS PONER A CERO CUANDO INICIALIZAMOS EL CROMOSOMA*/    
     
     
-    public static Cromosoma[] Cruzar(Cromosoma c1, Cromosoma c2){
-        Cromosoma cruzado1 = new Cromosoma();
-        Cromosoma cruzado2 = new Cromosoma();
+    public static Cromosoma[] Cruzar(Cromosoma c1, Cromosoma c2, Conocimiento entrada){
+        Cromosoma cruzado1 = new Cromosoma(entrada);
+        Cromosoma cruzado2 = new Cromosoma(entrada);
         Cromosoma[] cruceResult = new Cromosoma[2];
+        int corte = (int) c1.cantClientes/2;
+        int relleno = 0;
         
         // Realizar el cruce de c1 y c2, y producir el resultado en 
         // cruzado1 y cruzado2
+        for (int j = 0; j < c1.cantVehiculos; j++) {
+            for (int i = 0; i < c1.dias; i++) {
+                relleno =0;
+                for (int k = 1; k < c1.cantClientes + 1; k++) {
+                    if(k < corte){
+                        if(!revisarRepetido(cruzado1, c1.cromosoma[i][j][k])){
+                            cruzado1.cromosoma[i][j][k] = c1.cromosoma[i][j][k];
+                            cruzado1.listaVisitasCromo[1][k]--;
+                        }
+                        if(!revisarRepetido(cruzado2, c2.cromosoma[i][j][k])){
+                            cruzado2.cromosoma[i][j][k] = c2.cromosoma[i][j][k];
+                            cruzado2.listaVisitasCromo[1][k]--;
+                        }
+                        
+                    }else{
+                        relleno++;
+                        if(!revisarRepetido(cruzado1, c2.cromosoma[i][j][relleno])){
+                            cruzado1.cromosoma[i][j][k] = c2.cromosoma[i][j][relleno];
+                            cruzado1.listaVisitasCromo[1][relleno]--;
+                        }
+                        if(!revisarRepetido(cruzado2, c1.cromosoma[i][j][relleno])){
+                            cruzado2.cromosoma[i][j][k] = c1.cromosoma[i][j][relleno];
+                            cruzado2.listaVisitasCromo[1][relleno]--;
+                        }
+                    }
+                }
+            }
+        }
+        
         
         cruceResult[0] = cruzado1;
         cruceResult[1] = cruzado2;
         
         return cruceResult;
     }
+    
+    public void crucePorPunto
     
     public static Cromosoma Mutar (Cromosoma c1){
         Cromosoma mutado = new Cromosoma();
@@ -53,5 +86,10 @@ public class FuncionesGA {
          * 
          */
         return new Cromosoma[p.getTamanho()];
+    }
+    
+    public static boolean revisarRepetido(Cromosoma cromo, int nro){
+        
+        return true;
     }
 }
