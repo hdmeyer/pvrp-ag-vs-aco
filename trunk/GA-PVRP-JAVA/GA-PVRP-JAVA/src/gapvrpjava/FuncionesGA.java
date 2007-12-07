af2d620e5d8aeed3bf454eaa22d58faa157c3b6d
cdparra <cdparra@7294a8cf-013f-0410-b56a-9dc7521f48fc>
2007-12-07 01:13:20 +0000

    Modificaciones generales

--- trunk/GA-PVRP-JAVA/GA-PVRP-JAVA/src/gapvrpjava/FuncionesGA.java
+++ trunk/GA-PVRP-JAVA/GA-PVRP-JAVA/src/gapvrpjava/FuncionesGA.java
@@ -10,22 +10,48 @@
 package gapvrpjava;
 import java.util.*;
 /**
- *
- * @author Propietario
+ * Librería de funciones utilizadas para realizar las operaciones genéticas
+ * sobre los Cromosomas solución del problema PVRP
+ * 
+ * @author Cristhian Parra
+ * @author Hugo Meyer
  */
 public class FuncionesGA {
     /** Creates a new instance of FuncionesGA */
     LinkedList<Cromosoma> poblacion = new LinkedList<Cromosoma>();
-    
+        
     public FuncionesGA() {
     }
     /*PODRIAMOS PONER A CERO CUANDO INICIALIZAMOS EL CROMOSOMA*/    
     
     
-    public void cruce(){
-        ListIterator itr = poblacion.listIterator();
-        for (Cromosoma elem : poblacion) {
-            
-        }
+    public static ArrayList<Cromosoma> Cruce(Cromosoma c1, Cromosoma c2){
+        Cromosoma cruzado1 = new Cromosoma();
+        Cromosoma cruzado2 = new Cromosoma();
+        ArrayList<Cromosoma> cruceResult = new ArrayList();
+        
+        // Realizar el cruce de c1 y c2, y producir el resultado en 
+        // cruzado1 y cruzado2
+        
+        cruceResult.add(cruzado1);
+        cruceResult.add(cruzado2);
+        
+        return cruceResult;
+    }
+    
+    public static Cromosoma Mutar (Cromosoma c1){
+        Cromosoma mutado = new Cromosoma();
+        
+        // Operar sobre c1 para mutarlo y colocar el resultado en mutado
+        
+        return mutado;
+    }
+    
+    
+    public static void seleccion(Poblacion p) {
+        /** 
+         * @TODO 
+         * 
+         */
     }
 }
