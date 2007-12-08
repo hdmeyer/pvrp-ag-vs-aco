f086ddd25b37a13d498405c53637c95d57609a69
meyer.hugo <meyer.hugo@7294a8cf-013f-0410-b56a-9dc7521f48fc>
2007-12-08 03:32:15 +0000

    Corregido...

--- GA-PVRP-JAVA/GA-PVRP-JAVA/src/gapvrpjava/FuncionesGA.java
+++ GA-PVRP-JAVA/GA-PVRP-JAVA/src/gapvrpjava/FuncionesGA.java
@@ -31,6 +31,7 @@ public class FuncionesGA {
         int corte1;
         int corte2;
         int valor = 0;
+        int contador =0;
 
         // Realizar el cruce de c1 y c2, y producir el resultado en 
         // cruzado1 y cruzado2
@@ -48,27 +49,28 @@ public class FuncionesGA {
                     cruzado1.cromosoma[i][j].ruta.addAll(v1);
                     List<Integer> v2 = c2.cromosoma[i][j].ruta.subList(0, corte2);
                     cruzado2.cromosoma[i][j].ruta.addAll(v2);
-
+                    /*
                     List<Integer> v3 = c1.cromosoma[i][j].ruta.subList(corte1,c1.cromosoma[i][j].ruta.size());
                     List<Integer> v4 = c2.cromosoma[i][j].ruta.subList(corte2,c2.cromosoma[i][j].ruta.size());
+                    */
 
-
-                    Iterator<Integer> it1 = v3.iterator();
-                    Iterator<Integer> it2 = v4.iterator();
+                    Iterator<Integer> it1 = c1.cromosoma[i][j].ruta.iterator();
+                    Iterator<Integer> it2 = c2.cromosoma[i][j].ruta.iterator();
 
                     while(it1.hasNext()){
                         valor = (int)it1.next();
-                        if (!cruzado2.cromosoma[i][j].ruta.contains(valor)) {
+                        if (!cruzado2.cromosoma[i][j].ruta.contains(valor)&& contador <= corte1) {
                             cruzado2.cromosoma[i][j].ruta.add(valor);
-
+                            contador++;
                             cruzado2.listaVisitasCromo[1][valor]--;
                         }
                     }
-                    
+                    contador =0;
                     while(it2.hasNext()){
                         valor = (int)it2.next();
-                        if (!cruzado1.cromosoma[i][j].ruta.contains(valor)) {
+                        if (!cruzado1.cromosoma[i][j].ruta.contains(valor) && contador <= corte2) {
                             cruzado1.cromosoma[i][j].ruta.add(valor);
+                            contador++;
                             cruzado1.listaVisitasCromo[1][valor]--;
                         }
                     } 
@@ -93,6 +95,17 @@ public class FuncionesGA {
                         List<Integer> v2 = c2.cromosoma[i][j].ruta.subList(corte2, c2Size);
                         cruzado2.cromosoma[i][j].ruta.addAll(v2);
                     }                    
+                }
+                /*ACTUALIZAMOS LA CANTIDAD DE VECINOS*/
+                Iterator<Integer> it1 = cruzado1.cromosoma[i][j].ruta.iterator();
+                while (it1.hasNext()) {
+                    valor = (int) it1.next();
+                    cruzado1.listaVisitasCromo[1][valor]--;
+                }
+                Iterator<Integer> it2 = cruzado2.cromosoma[i][j].ruta.iterator();
+                while (it2.hasNext()) {
+                    valor = (int) it2.next();
+                    cruzado2.listaVisitasCromo[1][valor]--;
                 }
             }
         }
