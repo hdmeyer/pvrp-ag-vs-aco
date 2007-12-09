8b1be139e9cc487067091479e9de2e642b0e198f
cdparra <cdparra@7294a8cf-013f-0410-b56a-9dc7521f48fc>
2007-12-09 14:59:49 +0000

    Prueba sobre un cruce simple

---
+++ GA-PVRP-JAVA/GA-PVRP-JAVA/src/pruebas/TestCruceSimple.java
@@ -0,0 +1,69 @@
+/*
+ * TestCromosoma.java
+ *
+ * Created on 31 de octubre de 2007, 19:37
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package pruebas;
+
+import gapvrpjava.*;
+
+/**
+ *
+ * @author Huguis
+ */
+public class TestCruceSimple {
+    
+    /** Creates a new instance of TestCromosoma */
+    public TestCruceSimple() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception{
+        // TODO code application logic here
+        Conocimiento prueba= new Conocimiento();
+        String filename = "C:\\pvrp\\p26";
+        int ITERACIONES = 1;
+        
+     
+        prueba.CargarConocimiento(filename);                
+        System.out.println("1. PRUEBA DE OPERCIÓN DE CRUCE");
+        System.out.println("-* Población Inicial Generada =========================================");
+        
+        Poblacion p = new Poblacion(prueba, 2, 30);        
+        p.evaluar();
+        
+        String popString = p.toStringImprimible();        
+        System.out.print(popString);        
+                
+        // SELECCIÓN BÁSICA...        
+        int i = 0;
+        while (i<=ITERACIONES) {
+            i++;
+            
+            // SELECCIÓN ABSOLUTA
+            Cromosoma[] selectos = p.getIndividuos();
+            p.cruce(selectos);
+            //p.reemplazar();
+            p.setIndividuos(p.getHijos());
+            p.evaluar();        
+            popString = p.toStringImprimible();        
+            System.out.println(popString);
+            System.out.print("INVALIDOS: --> "+p.hayInvalidos()+": ");
+            
+            for (int j = 0; j < p.getInvalidos().length; j++) {
+                System.out.print(" - "+p.getInvalidos()[j]+" - ");                
+            }
+        }
+        
+        System.out.println("\n-* ("+i+") vueltas Población Cruzada  =========================================");
+                
+        
+    }
+    
+}
