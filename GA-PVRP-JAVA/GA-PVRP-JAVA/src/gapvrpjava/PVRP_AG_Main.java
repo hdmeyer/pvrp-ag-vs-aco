bce01f7a50864477fe7808e97788436f5c8794ff
cdparra <cdparra@7294a8cf-013f-0410-b56a-9dc7521f48fc>
2007-12-12 03:38:32 +0000

    Versión final del Main de AG, extendido para que realice en una sola ejecución, 10 corridas del algoritmo sobre la misma instancia del problema, y escriba los resultados en diferentes resultados.
    Algunos archivos de solución generados

--- GA-PVRP-JAVA/GA-PVRP-JAVA/src/gapvrpjava/PVRP_AG_Main.java
+++ GA-PVRP-JAVA/GA-PVRP-JAVA/src/gapvrpjava/PVRP_AG_Main.java
@@ -13,12 +13,12 @@ import java.io.FileWriter;
  * @author Cristhian Parra
  */
 public class PVRP_AG_Main {
-    private static String SOL_DIR = "D:\\cparra\\Cparra\\Estudios\\FACU\\2007\\8voSemestre\\ia\\tp-ia\\fuentes\\GA-PVRP-JAVA\\GA-PVRP-JAVA\\sols";
+    private static String SOL_DIR = "D:\\cparra\\Cparra\\Estudios\\FACU\\2007\\8voSemestre\\ia\\tp-ia\\fuentes\\GA-PVRP-JAVA\\GA-PVRP-JAVA\\sols\\";
 
     private static int CANT_GENERACIONES = 50000;
     private static int PROB_MUTACION = 30;
     private static int TAM_POBLACION = 40;
-    private static int MAX_TIEMPO = 5; // EN MINUTOS
+    private static int MAX_TIEMPO = 1; // EN MINUTOS
 
     /**
      * @param args the command line arguments
@@ -59,23 +59,7 @@ public class PVRP_AG_Main {
             testNumber = "PRUEBA";
         }
 
-
-        /*
-         * Variables para calculo de Resultados.
-         *  Correr por: 5 min (300 seg)
-         *  Tomar muestra cada: 5 seg.
-         */
-        long muestra = 5000; 
-        String[] tiempos = new String[70];
-        String[]  fitness = new String[70];
-        String[]  generaciones = new String[70];
-        tiempos[0] = "Tiempo";
-        fitness[0] = "Costo";
-        generaciones[0] = "Generacion";
-        int medida = 1;
         
-        long iteradorTiempo = muestra; // se evalua de a 5 segundos	
-        long maxTiempo = 60000L * MAX_TIEMPO;
         
         //impresión de título
         System.out.println("-----------------------------------------------------------------------");
@@ -91,105 +75,147 @@ public class PVRP_AG_Main {
         
         String conocimientoString = conocimiento.toString();
         System.out.println(conocimientoString);
-        
-        System.out.println("-------------------------------------------------------------------------");
-        System.out.println("II. INICALIZANDO POBLACIÓN \n");
-        
-        poblacion = new Poblacion(conocimiento,TAM_POBLACION,PROB_MUTACION);
-        poblacion.evaluar();
-        
-        System.out.println("  2.1. Población Inicial: \n");     
-        String popInicial = poblacion.toStringImprimible();        
-        System.out.println(popInicial);
-        
-        
-        System.out.println("-------------------------------------------------------------------------");
-        System.out.println("III. GENERACIONES \n");
-        
-
-        boolean parada = false;
 
         
-        // medida de tiempo
-        long tiempoActual;
-        long inicio = System.currentTimeMillis();
-                
-        while (!parada) {
-            String generacion = "--> POBGEN ["+poblacion.getGeneracion()+"]::";
+        System.out.println("-------------------------------------------------------------------------");
+        System.out.println("II. Inicio de Pruebas \n");
+        System.out.println("-------------------------------------------------------------------------");
+       
+        for (int prueba = 0; prueba < 10; prueba++) {
+        
+            System.out.println("############################# -- PRUEBA NRO. " + prueba + " -- #############################");
             
-            // iteraciones del algoritmo genetico
-            Cromosoma [] selectos = poblacion.seleccion();
-            poblacion.cruce(selectos);
-            
-            //poblacion.setHijos(poblacion.getIndividuos());
-            poblacion.mutar();
-            poblacion.reemplazar();
-            boolean newBestGlobal = poblacion.evaluar();
-            
-            System.out.println(" Inválidos en la Población -> "+poblacion.hayInvalidos());
-            System.out.println(generacion+"BEST Local  -> "+poblacion.toStringMejorActual());
-            System.out.println(generacion+"BEST Global -> "+poblacion.toStringMejorHistorico());
-            if (newBestGlobal) {
-                System.out.println(generacion+"NUEVO BEST Global!!!!");
+            /*
+             * Variables para calculo de Resultados.
+             *  Correr por: 5 min (300 seg)
+             *  Tomar muestra cada: 5 seg.
+             */
+            long muestra = 5000; 
+
+            long iteradorTiempo = muestra; // se evalua de a 5 segundos	
+            long maxTiempo = 10000L * MAX_TIEMPO;
+
+            int longSalida = (int)( maxTiempo / muestra) + 2;
+
+            String[] tiempos = new String[longSalida];
+            String[] fitness = new String[longSalida];
+            String[] fitnessLocales = new String[longSalida];
+            String[] generacionLocal = new String[longSalida];
+            String[] generaciones = new String[longSalida];
+            tiempos[0] = "Tiempo";
+            fitness[0] = "Costo";
+            generaciones[0] = "Generacion";
+            fitnessLocales[0] = "Costo Mejores Locales";
+            generacionLocal[0] = "Generacion Local";
+            int medida = 1;
+
+            String headPrueba = "####PRUEBA "+prueba+":: ";
+            System.out.println(headPrueba+":: INICALIZANDO POBLACIÓN... \n");
+
+            poblacion = new Poblacion(conocimiento,TAM_POBLACION,PROB_MUTACION);
+            poblacion.evaluar();
+
+            System.out.println("  2.1. Población Inicial: \n");     
+            String popInicial = poblacion.toStringImprimible();        
+            System.out.println(popInicial);
+
+
+            System.out.println("-------------------------------------------------------------------------");
+            System.out.println(headPrueba+"GENERACIONES \n");
+
+
+            boolean parada = false;
+
+
+            // medida de tiempo
+            long tiempoActual;
+            long inicio = System.currentTimeMillis();
+
+            while (!parada) {
+                String generacion = headPrueba+"--> POBGEN ["+poblacion.getGeneracion()+"]::";
+
+                // iteraciones del algoritmo genetico
+                Cromosoma [] selectos = poblacion.seleccion();
+                poblacion.cruce(selectos);
+
+                //poblacion.setHijos(poblacion.getIndividuos());
+                poblacion.mutar();
+                poblacion.reemplazar();
+                boolean newBestGlobal = poblacion.evaluar();
+
+                System.out.println(" Inválidos en la Población -> "+poblacion.hayInvalidos());
+                System.out.println(generacion+"BEST Local  -> "+poblacion.toStringMejorActual());
+                System.out.println(generacion+"BEST Global -> "+poblacion.toStringMejorHistorico());
+                if (newBestGlobal) {
+                    System.out.println(generacion+"NUEVO BEST Global!!!!");
+                }
+
+                //System.out.println(generacion+"POBLACIÓN GENERACIÓN: \n");
+                //String ultimo = poblacion.toStringImprimible();
+                //System.out.println(generacion+ultimo);
+                System.out.println(generacion+"END ----------------------------------------------------------->");
+
+                tiempoActual = System.currentTimeMillis();
+
+                if (tiempoActual - inicio >= maxTiempo)
+                        parada = true;
+                else if (tiempoActual - inicio >= iteradorTiempo){
+                        //System.out.println("Generacion: "+iteraciones);
+                        //imprimirMejor(poblacion);
+                        tiempos[medida] = ""+(tiempoActual - inicio);
+                        fitness[medida] = ""+poblacion.getMejorCostoHistorico();
+                        generaciones[medida] = ""+poblacion.getMejorGeneracion();
+                        fitnessLocales[medida] = ""+poblacion.getMejorCosto();
+                        generacionLocal[medida] = ""+poblacion.getGeneracion();                     
+                        
+                        medida++;
+                        iteradorTiempo += muestra; 		
+                }            
             }
 
-            //System.out.println(generacion+"POBLACIÓN GENERACIÓN: \n");
-            //String ultimo = poblacion.toStringImprimible();
-            //System.out.println(generacion+ultimo);
-            System.out.println(generacion+"END ----------------------------------------------------------->");
-            
-            tiempoActual = System.currentTimeMillis();
 
-            if (tiempoActual - inicio >= maxTiempo)
-                    parada = true;
-            else if (tiempoActual - inicio >= iteradorTiempo){
-                    //System.out.println("Generacion: "+iteraciones);
-                    //imprimirMejor(poblacion);
-                    tiempos[medida] = ""+(tiempoActual - inicio);
-                    fitness[medida] = ""+poblacion.getMejorCostoHistorico();
-                    generaciones[medida] = ""+poblacion.getMejorGeneracion();
-                    medida++;
-                    iteradorTiempo += muestra; 		
-            }            
-        }
-        
-        
-        System.out.println("-------------------------------------------------------------------------");
-        System.out.println("IV. FIN DEL ALGORITMO \n");
-        System.out.println("Mejor Solución Global -> "+poblacion.toStringMejorHistorico());
-        System.out.println("Generación de la Mejor Solución Global -> " + poblacion.getMejorGeneracion());
-        System.out.println(" Es la mejor solución válida? "+poblacion.getMejorHistorico().isValido(conocimiento));
-        System.out.println(" Costo de la Mejor solución Conocida: "+conocimiento.mejorSol);
-        double relacionMejorSol = conocimiento.mejorSol/poblacion.getMejorCostoHistorico();
-        System.out.println(" Mejor solución conocida / Mejor solución encontrada: "+relacionMejorSol);
-        
-        System.out.println("-------------------------------------------------------------------------");
-        
-        System.out.println("Escribiendo resultados a archivo...");
-        
-        CSVWriter writer = null;
-        try {
-                String path = SOL_DIR+instanceNumber+"_SOL"+testNumber+"_"+
-                        TAM_POBLACION+"IND_"+MAX_TIEMPO+"MIN_"+PROB_MUTACION+"MUT.csv";
-                writer = new CSVWriter(new FileWriter(path));
-        } catch (IOException e){
-                System.out.println("Error de escritura del archivo historico");
+            System.out.println(headPrueba+"-------------------------------------------------------------------------");
+            System.out.println(headPrueba+"FIN DEL ALGORITMO \n");
+            System.out.println(headPrueba+"FIN -> Mejor Solución Global -> "+poblacion.toStringMejorHistorico());
+            System.out.println(headPrueba+"FIN -> Generación de la Mejor Solución Global -> " + poblacion.getMejorGeneracion());
+            System.out.println(headPrueba+"FIN -> Es la mejor solución válida? "+poblacion.getMejorHistorico().isValido(conocimiento));
+            System.out.println(headPrueba+"FIN -> Costo de la Mejor solución Conocida: "+conocimiento.mejorSol);
+            double relacionMejorSol = conocimiento.mejorSol/poblacion.getMejorCostoHistorico();
+            System.out.println(headPrueba+"FIN -> Mejor solución conocida / Mejor solución encontrada: "+relacionMejorSol);
+
+            System.out.println("-------------------------------------------------------------------------");
+
+            System.out.println("Escribiendo resultados a archivo...");
+
+            CSVWriter writer = null;
+            try {
+                    String path = SOL_DIR+instanceNumber+"_TEST"+prueba+"_"+
+                            TAM_POBLACION+"IND_"+MAX_TIEMPO+"MIN_"+PROB_MUTACION+"MUT.csv";
+                    writer = new CSVWriter(new FileWriter(path));
+            } catch (IOException e){
+                    System.out.println("Error de escritura del archivo historico");
+                    e.printStackTrace();
+                    System.exit(0);			
+            }
+
+            writer.writeNext(tiempos);
+            writer.writeNext(fitness);
+            writer.writeNext(generaciones);
+            writer.writeNext(fitnessLocales);
+            writer.writeNext(generacionLocal);
+
+            try {
+                writer.flush();
+                writer.close();	
+            } catch (IOException e){
+                System.out.println("Error para cerrar archivo historico");
                 e.printStackTrace();
                 System.exit(0);			
-        }
-        
-        writer.writeNext(tiempos);
-        writer.writeNext(fitness);
-        writer.writeNext(generaciones);
-
-        try {
-            writer.close();	
-        } catch (IOException e){
-            System.out.println("Error para cerrar archivo historico");
-            e.printStackTrace();
-            System.exit(0);			
-        }		
-		
+            }		
+            
+            
+            System.out.println("######################## -- PRUEBA NRO. " + prueba + " FINALIZADO -- ########################");
+       }		
         
         /*
         System.out.println("-------------------------------------------------------------------------");
