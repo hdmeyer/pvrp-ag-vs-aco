c0e281884f4dbc135c03f50f74eda253b9e35149
cdparra <cdparra@7294a8cf-013f-0410-b56a-9dc7521f48fc>
2007-12-12 02:08:17 +0000

    Pequeños cambios para mejorar los archivos de salida

--- GA-PVRP-JAVA/GA-PVRP-JAVA/src/gapvrpjava/PVRP_AG_Main.java
+++ GA-PVRP-JAVA/GA-PVRP-JAVA/src/gapvrpjava/PVRP_AG_Main.java
@@ -13,10 +13,12 @@ import java.io.FileWriter;
  * @author Cristhian Parra
  */
 public class PVRP_AG_Main {
+    private static String SOL_DIR = "D:\\cparra\\Cparra\\Estudios\\FACU\\2007\\8voSemestre\\ia\\tp-ia\\fuentes\\GA-PVRP-JAVA\\GA-PVRP-JAVA\\sols";
 
     private static int CANT_GENERACIONES = 50000;
     private static int PROB_MUTACION = 30;
     private static int TAM_POBLACION = 40;
+    private static int MAX_TIEMPO = 5; // EN MINUTOS
 
     /**
      * @param args the command line arguments
@@ -27,25 +29,34 @@ public class PVRP_AG_Main {
         Conocimiento conocimiento;
         String problemInstanceFile;
         String bestSolutionFile;
-        String solucionNro;
+        String testNumber;        
+        String instanceNumber;
+        String solutionNumber;
        
         /**
          * lectura de parámatros iniciales, preparación del algoritmo. 
          * 
          */
+        
         if (args.length == 3) {
             problemInstanceFile = args[0];
             bestSolutionFile = args[1];
-            solucionNro = args[2];
+            testNumber = args[2];
+            instanceNumber = args[0];
+            solutionNumber = args[1];
             
         } else if (args.length == 2) {
+            instanceNumber = args[0];
+            solutionNumber = args[0];
             problemInstanceFile = "C:\\pvrp\\p"+args[0];
             bestSolutionFile = "C:\\pvrp-sols\\p"+args[0]+".res";
-            solucionNro = args[1];
+            testNumber = args[1];
         } else {
             problemInstanceFile = "C:\\pvrp\\p26";
             bestSolutionFile = "C:\\pvrp-sols\\p26.res";
-            solucionNro = "PRUEBA";
+            instanceNumber = "26";
+            solutionNumber = "26";
+            testNumber = "PRUEBA";
         }
 
 
@@ -64,7 +75,7 @@ public class PVRP_AG_Main {
         int medida = 1;
         
         long iteradorTiempo = muestra; // se evalua de a 5 segundos	
-        long maxTiempo = 60000L * 5;
+        long maxTiempo = 60000L * MAX_TIEMPO;
         
         //impresión de título
         System.out.println("-----------------------------------------------------------------------");
@@ -158,7 +169,8 @@ public class PVRP_AG_Main {
         
         CSVWriter writer = null;
         try {
-                String path = problemInstanceFile+"_SOL"+solucionNro+"_30_5MIN.csv";
+                String path = SOL_DIR+instanceNumber+"_SOL"+testNumber+"_"+
+                        TAM_POBLACION+"IND_"+MAX_TIEMPO+"MIN_"+PROB_MUTACION+"MUT.csv";
                 writer = new CSVWriter(new FileWriter(path));
         } catch (IOException e){
                 System.out.println("Error de escritura del archivo historico");
